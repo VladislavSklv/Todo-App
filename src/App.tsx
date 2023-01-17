70094c0d5e4c02cbb19b3f9e960829f673054f49
Vladislav Sokolov <79214579055@yandex.ru>
2023-01-17 22:15:33 +0300

    started projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Stack, TextField, Button } from '@mui/material';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [query, setQuery] = useState('');
+
+	return (
+		<div className='App'>
+			<Stack direction={'row'} spacing={2}>
+				<TextField 
+					id="outlined-basic" 
+					label="Search" 
+					variant="outlined"
+					value={query}
+					onChange={(e) => setQuery(e.target.value)} 
+				/>
+				<Button variant="contained" color="success">
+					<span style={{fontSize: 30, lineHeight: 1}}>+</span>
+				</Button>
+			</Stack>
+		</div>
+	);
 }
 
 export default App;
